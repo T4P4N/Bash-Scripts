51c5c8d765887a75d05c030acd26e87265bdd664
T4P4N <32695903+T4P4N@users.noreply.github.com>
2018-03-18 16:32:25 +0530

    Update Dependencies.sh
    added more modules

--- Dependencies.sh
+++ Dependencies.sh
@@ -1,9 +1,15 @@
 #!/data/data/com.termux/files/usr/bin/sh
 apt install -y autoconf automake bison bzip2 clang cmake coreutils diffutils flex gawk grep gzip libtool make patch perl sed silversearcher-ag tar wget
-cpan install Log::Log4perl
+cpan install Getopt::Long
 cpan install HTTP::Request
-cpan install LWP::Useragent
-cpan install JSON
+cpan install LWP::UserAgent
+cpan install IO::Select
+cpan install HTTP::Cookies
+cpan install HTTP::Response
+cpan install Term::ANSIColor
+cpan install URI::URL
+cpan install IO::Socket::INET
+cpan install Try::Tiny
 sleep 2
 clear 
 echo"All the dependencies are installed !"
