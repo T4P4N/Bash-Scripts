1343f2d80facab04ae2c773f654e91e0d64e7f8c
Tapan Kashyap <32695903+T4P4N@users.noreply.github.com>
2018-11-18 05:08:44 +0000

    Add files via upload
    Tiny script to install exiftool

---
+++ exiftool_installer.sh
@@ -0,0 +1,15 @@
+apt install perl wget gzip make 
+cd $HOME
+sleep 1
+echo "Downloading tarball"
+sleep 1
+# wget https://sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-11.19.tar.gz
+sleep 1
+echo "Extracting Tarball"
+# gzip -dc Image-ExifTool-*.tar.gz | tar -xf -
+cd Image-ExifTool-11.19
+sleep 1
+echo "Installing Exiftool"
+perl Makefile.PL
+make install
+echo "Done!"
