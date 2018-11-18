dfbdfb2023d73170f45956d80016736c91abc3c5
Tapan Kashyap <32695903+T4P4N@users.noreply.github.com>
2018-11-18 05:09:26 +0000

    Update exiftool_installer.sh

--- exiftool_installer.sh
+++ exiftool_installer.sh
@@ -3,10 +3,10 @@ cd $HOME
 sleep 1
 echo "Downloading tarball"
 sleep 1
-# wget https://sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-11.19.tar.gz
+wget https://sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-11.19.tar.gz
 sleep 1
 echo "Extracting Tarball"
-# gzip -dc Image-ExifTool-*.tar.gz | tar -xf -
+gzip -dc Image-ExifTool-*.tar.gz | tar -xf -
 cd Image-ExifTool-11.19
 sleep 1
 echo "Installing Exiftool"
